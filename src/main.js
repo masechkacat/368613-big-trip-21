d66d34df72e335604eed7f8cdfea5e16c60c0c19
Marina Torunova <masechkacat@gmail.com>
2023-08-09 10:38:23 +0200

    view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import FilterView from './view/filter-view.js';
 import {render} from './render.js';
 
-const siteMainElement = document.querySelector('.trip-controls__filters');
+const siteHeaderContainer = document.querySelector('.page-header');
+const siteFilterControls = siteHeaderContainer.querySelector('.trip-controls__filters');
 
-render(new FilterView(), siteMainElement);
+render(new FilterView(), siteFilterControls);
