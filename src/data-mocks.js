9385ade431cffc8918ccc7fbd0de74e4a371d17d
Marina Torunova <masechkacat@gmail.com>
2023-08-11 19:41:51 +0200

    amelioreted mocks

--- src/data-mocks.js
+++ src/data-mocks.js
@@ -1,49 +1,68 @@
-import { pointTypes, cities, DESCRIPTIONS } from './const.js';
-import { getRandomInteger, getRandomDescription } from './utils.js';
+import dayjs from 'dayjs';
+import { getRandomArrayElement, generateID, getRandomInteger } from './utiles';
 
-const generateMockPoint = () => {
-  const id = `point-${getRandomInteger(1, 100)}`;
-  const type = pointTypes[getRandomDescription(Object.keys(pointTypes))];
-  const city = cities[getRandomInteger(0, cities.length - 1)];
-  const description = getRandomDescription(DESCRIPTIONS);
 
-  return {
-    id,
-    type,
-    city,
-    description
-  };
+// Генерация данных
+const CITY_DESCRIPTIONS = {
+  'Paris': 'Paris, known for its romantic ambiance, and its status as the world\'s fashion capital.',
+  'London': 'London, a historic city blending modernity and traditions.',
+  'New York': 'New York, the city that never sleeps and an epicenter of arts.',
+  'Tokyo': 'Tokyo, a bustling hub of innovation and ancient traditions.',
+  'Chamonix': 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.'
 };
 
-const generateMockDestination = () => {
-  const id = `destination-${getRandomInteger(1, 100)}`;
-  const name = cities[getRandomInteger(0, cities.length - 1)];
-  const description = getRandomDescription(DESCRIPTIONS);
+const POINT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight'];
 
+const OFFERS = {
+  'taxi': [
+    { id: generateID(), title: 'Upgrade to a business class', price: 120 },
+    { id: generateID(), title: 'Choose the radio station', price: 60 },
+    { id: generateID(), title: 'Drive quickly, I am in a hurry', price: 100 }
+  ],
+  'flight': [
+    {id: generateID(), title: 'Choose a sit', price: 50},
+    {id: generateID(), title: 'Add extra baggage', price: 70},
+    {id: generateID(), title: 'Switch to business class', price: 200}
+  ],
+  'train': [
+    {id: generateID(), title: 'First class seat', price: 80},
+    {id: generateID(), title: 'Add a meal', price: 20},
+    {id: generateID(), title: 'WiFi access', price: 5}
+  ]
+};
+
+const generatePictures = () => ({
+  src: `https://loremflickr.com/248/152?random=${getRandomInteger(1, 1000)}`,
+  description: getRandomArrayElement(['nature', 'city', 'sea', 'mountaines'])
+});
+
+
+const generateDestination = () => {
+  const name = getRandomArrayElement(Object.keys(CITY_DESCRIPTIONS));
   return {
-    id,
+    id: generateID(),
+    description: CITY_DESCRIPTIONS[name],
     name,
-    description
+    pictures: generatePictures()
   };
 };
 
-const generateMockOffer = (type) => {
-  const id = `offer-${type}-${getRandomInteger(1, 100)}`;
-  const title = getRandomDescription(DESCRIPTIONS);
-  const price = getRandomInteger(10, 100);
-
+const generatePoint = () => {
+  const type = getRandomArrayElement(POINT_TYPES);
   return {
-    id,
-    title,
-    price
+    id: generateID(),
+    basePrice: getRandomInteger(500, 2000),
+    dateFrom: dayjs().add(getRandomInteger(1, 10), 'day').toISOString(),
+    dateTo: dayjs().add(getRandomInteger(11, 20), 'day').toISOString(),
+    destination: generateDestination().id,
+    isFavorite: Boolean(getRandomInteger(0, 1)),
+    offers: OFFERS[type].map((offer) => offer.id),
+    type
   };
 };
 
-export const MOCK_OFFERS = Object.keys(pointTypes).map((type) => ({
-  type,
-  offers: Array.from({ length: 2 }, () => generateMockOffer(type)),
-}));
+const points = new Array(10).fill().map(generatePoint);
+const destinations = new Array(10).fill().map(generateDestination);
+const offers = POINT_TYPES.map((type) => ({ type, offers: OFFERS[type] }));
 
-export const MOCK_DESTINATIONS = Array.from({ length: 5 }, generateMockDestination);
-
-export const MOCK_POINTS = Array.from({ length: 10 }, generateMockPoint);
+export { points, destinations, offers };
--- src/utiles.js
+++ src/utiles.js
@@ -1,2 +1,11 @@
-export const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-export const getRandomDescription = (array) => array[getRandomInteger(0, array.length - 1)];
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+const generateID = () => Math.random().toString(36).substring(2, 10);
+
+export {getRandomInteger, getRandomArrayElement, generateID};
