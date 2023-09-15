3fd3b6c0b5541d3229c60848a9228479a485836e
Marina Torunova <masechkacat@gmail.com>
2023-09-15 10:27:46 +0200

    some fix

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -15,7 +15,8 @@ function createTripInfoTemplate(totalSumm, namesDestinations, datesTrip){
     <div class="trip-info__main">
       <h1 class="trip-info__title">${createRouteTemplate(namesDestinations)}</h1>
 
-      <p class="trip-info__dates">${formatDate(datesTrip.tripDateFrom, FormatsDate.MONTHDAY)}&nbsp;&mdash;&nbsp;${formatDate(datesTrip.tripDateTo, FormatsDate.MONTHDAY)}</p>
+      <p class="trip-info__dates">${formatDate(datesTrip.tripDateFrom, FormatsDate.MONTHDAY)}
+      &nbsp;&mdash;&nbsp;${formatDate(datesTrip.tripDateTo, FormatsDate.MONTHDAY)}</p>
     </div>
 
     <p class="trip-info__cost">
@@ -41,26 +42,3 @@ export default class TripInfoView extends AbstractView {
     return createTripInfoTemplate(this.#totalSumm, this.#namesDestinations, this.#datesTrip);
   }
 }
-
-/*function createTripInfoTemplate() {
-  return (
-    `<section class="trip-main__trip-info  trip-info">
-    <div class="trip-info__main">
-      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-    </div>
-
-    <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-    </p>
-  </section>
-`
-  );
-}
-
-export default class TripInfoView extends AbstractView {
-  get template() {
-    return createTripInfoTemplate();
-  }
-}*/
