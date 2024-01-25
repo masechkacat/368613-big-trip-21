8012dd6a50c1ebc27ae52af5d6bc990284ba9c95
Marina Torunova <masechkacat@gmail.com>
2024-01-25 10:10:49 +0100

    for deploy

--- src/view/loading-view.js
+++
@@ -1,8 +0,0 @@
-import AbstractView from '../framework/view/abstract-view.js';
-
-const createLoadingViewTemplate = () => '<p class="trip-events__msg">Loading...</p>';
-export default class LoadingView extends AbstractView {
-  get template() {
-    return createLoadingViewTemplate();
-  }
-}
